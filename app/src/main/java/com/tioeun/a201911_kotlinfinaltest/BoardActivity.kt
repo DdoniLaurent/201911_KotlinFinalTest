9d2ddb99abf9f152c2ce6e5a3cf6c71c68f35ec1
YongMinSun <ddoni1010@gmail.com>
2019-11-27 20:07:32 +0900

    2개의 프레그먼트를 ViewPager에 실어서 표기.

--- app/src/main/java/com/tioeun/a201911_kotlinfinaltest/BoardActivity.kt
+++ app/src/main/java/com/tioeun/a201911_kotlinfinaltest/BoardActivity.kt
@@ -3,7 +3,9 @@ package com.tioeun.a201911_kotlinfinaltest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.tioeun.a201911_kotlinfinaltest.adapters.BoardViewPagerAdapter
 import com.tioeun.a201911_kotlinfinaltest.utils.GlobalData
+import kotlinx.android.synthetic.main.activity_board.*
 
 class BoardActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,6 +20,9 @@ class BoardActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+        myViewPager.adapter = BoardViewPagerAdapter(supportFragmentManager)
+
 //        토스트로 로그인한 사람의 이름을 띄워주자
         if(GlobalData.loginUserData != null) {
             Toast.makeText(mContext, GlobalData.loginUserData!!.name, Toast.LENGTH_SHORT).show()
---
+++ app/src/main/java/com/tioeun/a201911_kotlinfinaltest/adapters/BoardViewPagerAdapter.kt
@@ -0,0 +1,30 @@
+package com.tioeun.a201911_kotlinfinaltest.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.tioeun.a201911_kotlinfinaltest.fragments.BoardListFragment
+import com.tioeun.a201911_kotlinfinaltest.fragments.NoticeListFragment
+
+class BoardViewPagerAdapter(fm:FragmentManager) : FragmentPagerAdapter(fm) {
+    override fun getItem(position: Int): Fragment {
+//        JAVA
+//        if(position == 0) {
+//            return BoardListFragment()
+//        } else {
+//            return NoticeListFragment()
+//        }
+//        KOTLIN
+        return when(position) {
+            0 -> {
+                BoardListFragment()
+            } else -> {
+                NoticeListFragment()
+            }
+        }
+    }
+
+    override fun getCount(): Int {
+        return 2
+    }
+}
